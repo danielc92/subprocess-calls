4d46798230fbfe2eaaf449cdf965ef69192c4ea5
Daniel Corcoran <danielc.92@outlook.com>
2019-03-05 15:12:30 +1100

    Created ff938623-64ee-4df3-a7f1-b608e0bc7383html

---
+++ main.py
@@ -0,0 +1,27 @@
+from subprocess import call
+from uuid import uuid4
+from time import sleep
+
+def create_filename():
+    filename = str(uuid4()) + 'html'
+    print('Filename assigned {}'.format(filename))
+    return filename
+
+# Create a file
+def add_file(filename):
+    call(['touch', filename])
+    print('Created filename {}'.format(filename))
+
+# Push
+def push(filename):
+    call(['git', 'add', '.'])
+    call(['git', 'commit', '-m', 'Created {}'.format(filename)])
+    call(['git', 'push', 'origin', 'master'])
+    print('Pushed to remote repo...')
+
+seconds = 10
+for x in range(10):
+    sleep(seconds)
+    filename = create_filename()
+    add_file(filename)
+    push(filename)
